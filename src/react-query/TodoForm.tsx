868986582462fe130c2c02370ab8ed30049e26aa
qiuxan <qiuxan@gmail.com>
2023-07-19 22:32:08 +1000

    002-16- Handling Mutation Errors

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -6,16 +6,12 @@ import axios from "axios";
 const TodoForm = () => {
   const queryClient = useQueryClient();
   const ref = useRef<HTMLInputElement>(null);
-  const addTodo = useMutation({
+  const addTodo = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
     onSuccess: (savedTodo, newTodo) => {
-      // console.log({ savedTodo });
-      // queryClient.invalidateQueries({
-      //   queryKey: ["todos"],
-      // });
       queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
         savedTodo,
         ...(todos || []),
@@ -23,27 +19,32 @@ const TodoForm = () => {
     },
   });
   return (
-    <form
-      className="row mb-3"
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (ref.current && ref.current.value) {
-          addTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            userId: 1,
-            completed: false,
-          });
-        }
-      }}
-    >
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
+      <form
+        className="row mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (ref.current && ref.current.value) {
+            addTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              userId: 1,
+              completed: false,
+            });
+          }
+        }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </form>
+    </>
   );
 };
 
