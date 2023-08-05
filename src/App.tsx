667dad1c1c22c779cbf7e68a3156b88811ba7cfa
qiuxan <qiuxan@gmail.com>
2023-08-05 12:19:13 +1000

    003-3- Creating Complex Actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "./App.css";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
   return (
     <>
       {/* <TodoForm />
       <TodoList /> */}
-      <Counter />
+      {/* <Counter /> */}
+      <TaskList />
     </>
   );
 }
