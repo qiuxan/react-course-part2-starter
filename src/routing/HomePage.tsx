28b885ed0422c86f9ad526fc9d1befbafcf7d0f2
qiuxan <qiuxan@gmail.com>
2023-08-05 23:13:07 +1000

    003-5- Sharing State using React Context

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,11 +1,14 @@
+import TaskList from "../state-management/TaskList";
+
 const HomePage = () => {
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+        mollitia!
       </p>
       <a href="/users">Users</a>
+      <TaskList />
     </>
   );
 };
