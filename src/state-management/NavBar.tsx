f3a61e289457109d4369cde42ad915cb7e6b3c51
qiuxan <qiuxan@gmail.com>
2023-08-07 18:02:34 +1000

    003-16- Managing Application State with Zustand

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,11 +1,13 @@
 import { LoginStatus } from "./auth";
+import useCounterStore from "./counter/store";
 import { useTasks } from "./tasks/TaskList";
 
 const NavBar = () => {
   const { tasks } = useTasks();
+  const { counter } = useCounterStore();
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">{counter}</span>
       <LoginStatus />
     </nav>
   );
--- src/state-management/counter/Counter.tsx
+++ src/state-management/counter/Counter.tsx
@@ -1,23 +1,16 @@
 import { useReducer, useState } from "react";
-import counterReducer from "./counterReducer";
+import useCounterStore from "./store";
 
 const Counter = () => {
-  // const [value, setValue] = useState(0);
-  const [value, dispatch] = useReducer(counterReducer, 0);
+  const { counter, increment, reset } = useCounterStore();
 
   return (
     <div>
-      Counter ({value})
-      <button
-        onClick={() => dispatch({ type: "INCREMENT" })}
-        className="btn btn-primary mx-1"
-      >
+      Counter ({counter})
+      <button onClick={() => increment()} className="btn btn-primary mx-1">
         Increment
       </button>
-      <button
-        onClick={() => dispatch({ type: "RESET" })}
-        className="btn btn-primary mx-1"
-      >
+      <button onClick={() => reset()} className="btn btn-primary mx-1">
         Reset
       </button>
     </div>
