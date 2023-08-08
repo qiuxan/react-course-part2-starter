4c8da8cd0ef3fe3277cc2bcd80a0824e43a96ff0
qiuxan <qiuxan@gmail.com>
2023-08-08 22:08:31 +1000

    Getting Data about the Current Route

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
+import UserDetailPage from "./UserDetailPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/users", element: <UserListPage /> },
+  { path: "/users/:id", element: <UserDetailPage /> },
 ]);
 
 export default router;
